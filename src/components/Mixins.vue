ab68405c1ed7289cde14779dd5727b3df17337e3
Alice <1356934076@qq.com>
2020-09-23 15:11:29 +0800

    mixin文件的更改

---
+++ src/components/Mixins.vue
@@ -0,0 +1,20 @@
+<template>
+<div>mixins</div>
+</template>
+
+<script>
+import {
+    mixins
+} from '../mixins/mixins'
+export default {
+    mixins: [mixins],
+
+    created() {
+        console.log('正常的created')
+    },
+
+    mounted() {
+        console.log('正常的mounted')
+    },
+}
+</script>
